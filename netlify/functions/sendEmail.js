2a3e9be3156b2a3b1a9e6078b679b8b4ec49983e
Yuri Alves <yurialvesguernsey@outlook.com>
2023-12-03 19:00:40 +0000

    init

--- netlify/functions/sendEmail.js
+++ netlify/functions/sendEmail.js
@@ -1,45 +1,42 @@
 const axios = require('axios');
 const sgMail = require('@sendgrid/mail');
 
-exports.handler = async (event) => {
-    if (event.httpMethod !== 'POST') {
-        return { statusCode: 405, body: 'Method Not Allowed' };
+exports.handler = async function(event, context){
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' };
+  }
+
+  const { email, message, recaptcha } = JSON.parse(event.body);
+
+  // Verify reCAPTCHA
+  const recaptchaSecret = process.env.RECAPTCHA_SECRET_KEY;
+  try {
+    const recaptchaResponse = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecret}&response=${recaptcha}`);
+    if (!recaptchaResponse.data.success || recaptchaResponse.data.score < 0.5) {
+      return { statusCode: 500, body: 'reCAPTCHA verification failed' };
     }
+  } catch (error) {
+    return { statusCode: 500, body: 'Error verifying reCAPTCHA' };
+  }
 
-    const { email, message, recaptcha } = JSON.parse(event.body);
+  // SendGrid setup
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-    // Verify reCAPTCHA v3
-    const recaptchaSecret = process.env.RECAPTCHA_SECRET_KEY; // Set this in Netlify environment variables
-    try {
-        const recaptchaResponse = await axios.post(`https://www.google.com/recaptcha/api/siteverify?secret=${recaptchaSecret}&response=${recaptcha}`);
-        if (!recaptchaResponse.data.success || recaptchaResponse.data.score < 0.5) { // Adjust score threshold as needed
-            return { statusCode: 500, body: 'reCAPTCHA verification failed' };
-        }
-    } catch (error) {
-        return { statusCode: 500, body: 'Error verifying reCAPTCHA' };
+  const msg = {
+    to: email,
+    from: process.env.SENDGRID_SENDER_EMAIL,
+    subject: 'Message from Astro Forum',
+    text: message,
+  };
+
+  try {
+    await sgMail.send(msg);
+    return { statusCode: 200, body: JSON.stringify({ message: 'Email sent successfully' }) };
+  } catch (error) {
+    console.error(error);
+    if (error.response) {
+      console.error(error.response.body);
     }
-
-    // SendGrid setup
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY); // Set in Netlify environment variables
-
-    const msg = {
-        to: email, // Change to your recipient
-        from: process.env.SENDGRID_SENDER_EMAIL, // Change to your verified sender
-        subject: 'Message from Astro Forum',
-        text: message,
-        // html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-    };
-
-    try {
-        await sgMail.send(msg);
-        return { statusCode: 200, body: JSON.stringify({ message: 'Email sent successfully' }) };
-    } catch (error) {
-        console.error(error);
-
-        if (error.response) {
-            console.error(error.response.body)
-        }
-
-        return { statusCode: 500, body: JSON.stringify({ message: 'Error sending email' }) };
-    }
+    return { statusCode: 500, body: JSON.stringify({ message: 'Error sending email' }) };
+  }
 };
